79ced7a8db1d02cd42f890479062d5c6a6b5726b
kosuke75 <kousuke_2527.1@icloud.com>
2025-01-11 00:56:10 +0900

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function NavbarComponent() {
     const handleShow = () => setShow(true);
 
     const checkout = async () => {
-        const response = await fetch('https://main.d1i2hzm1xh2h9b.amplifyapp.com/checkout', {
+        const response = await fetch('http://3.107.22.67:4000/checkout', {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
