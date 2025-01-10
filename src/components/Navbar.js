d74aff2e14e51211b7c7480fbdb9c4fdaac87212
kosuke75 <kousuke_2527.1@icloud.com>
2025-01-11 02:35:54 +0900

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function NavbarComponent() {
     const handleShow = () => setShow(true);
 
     const checkout = async () => {
-        const response = await fetch('https://order-servertest.zapto.org', {
+        const response = await fetch('http://3.107.22.67/', {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
